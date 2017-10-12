8bea31081ca32251c1c1b570945217c02fa623c6
nakanoya150151 <nakanoya150151@simplex.ne.jp>
2017-10-12 17:28:32 +0900

    bitcoinjのmodel使用に伴いコピークラスを削除

--- src/main/java/co/jp/simplex/siw/job/zmq/ZmqSample.java
+++ src/main/java/co/jp/simplex/siw/job/zmq/ZmqSample.java
@@ -1,6 +1,5 @@
 package co.jp.simplex.siw.job.zmq;
 
-import co.jp.simplex.siw.bitcoinj.BtcRawDataParser;
 import co.jp.simplex.siw.utils.AppUtils;
 import org.bitcoinj.core.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,25 +16,22 @@ public class ZmqSample {
     private static final String url = "tcp://127.0.0.1:28332";
 
     @Autowired
-    private BtcRawDataParser btcRawDataParser;
-
-    @Autowired
     private AppUtils appUtils;
 
     @PostConstruct
-    private void init(){
-      
+    private void init() {
+
         ExecutorService executor = Executors.newFixedThreadPool(1);
         try {
-          executor.execute(command);
+            executor.execute(command);
         } finally {
-          executor.shutdown();
-          try {
-			executor.awaitTermination(1, TimeUnit.MINUTES);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            executor.shutdown();
+            try {
+                executor.awaitTermination(1, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
     }
@@ -44,25 +40,22 @@ public class ZmqSample {
         public void run() {
             ZMQ.Context context = ZMQ.context(1);
             ZMQ.Socket socket = context.socket(ZMQ.SUB);
-            
-    
+
+
             socket.connect(url);
             socket.subscribe("rawblock");
             // socket.subscribe("hashblock");
             socket.subscribe("rawtx");
             // socket.subscribe("hashtx");
-            System.out.println("connect to " + url );
-    
-            while(!Thread.currentThread().isInterrupted()) {
+            System.out.println("connect to " + url);
+
+            while (!Thread.currentThread().isInterrupted()) {
                 String topic = socket.recvStr();
                 System.out.println("client - topic:" + topic);
                 if (socket.hasReceiveMore()) {
                     byte[] body = socket.recv();
                     try {
                         Transaction tx = new Transaction(appUtils.getNetWorkParam(), body);
-//                        BtcTransaction tx = btcRawDataParser.parseTx(msg2);
-//                    String strmsg2 = BinAscii.hexlify(msg2);
-                        // String strmsg2 = BinAscii.hexlify(Arrays.copyOfRange(msg2, 0, 80));
                         System.out.println("client - body:" + tx.toString());
                     } catch (Exception e) {
                         // TODO Auto-generated catch block
